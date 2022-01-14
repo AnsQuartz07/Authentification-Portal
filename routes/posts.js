c88628ee636660e644d900f6c06734c3f971dc32
Anshu Kumar <97735640+AnsQuartz07@users.noreply.github.com>
2022-01-15 05:19:08 +0530

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -3,8 +3,8 @@ const verify = require('./verifyToken');
    router.get('/', verify, (req,res) => {
     res.json({
         posts: {
-            title: 'Priya kumari', 
-            description: 'I am the cutest girl in india.'
+            title: 'IIT Patna', 
+            description: 'Indian Institute of Technology Patna (abbreviated IIT Patna or IITP) is a public technical university located in Patna, India. It is recognized as an Institute of National Importance by the Government of India. It is one of the new IITs established by an Act of the Indian Parliament on August 6, 2008. The permanent campus of IIT Patna is located at Bihta which is approximately 30 km from Patna and has been fully operational since 2015.'
         }
 
    });
