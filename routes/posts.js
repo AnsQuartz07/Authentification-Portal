db6d5f2b990daca3b0f7183f218ce40bcd3ef078
Anshu Kumar <97735640+AnsQuartz07@users.noreply.github.com>
2022-02-16 16:23:15 +0530

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,7 @@ const verify = require('./verifyToken');
    router.get('/', verify, (req,res) => {
     res.json({
         posts: {
-            title: 'IIT Patna', 
+            title: ' Patna', 
             description: 'Indian Institute of Technology Patna (abbreviated IIT Patna or IITP) is a public technical university located in Patna, India. It is recognized as an Institute of National Importance by the Government of India. It is one of the new IITs established by an Act of the Indian Parliament on August 6, 2008. The permanent campus of IIT Patna is located at Bihta which is approximately 30 km from Patna and has been fully operational since 2015.'
         }
 
